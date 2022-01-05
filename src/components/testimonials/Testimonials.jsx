9d35cb2c134e19966eecba9f965faaee021b2abf
surya1332002 <suryavardhanreddy13@gmail.com>
2022-01-06 00:10:36 +0530

    first commit

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,21 @@
+import "./testimonials.scss"
+
+export default function Testimonials() {
+    return (
+        <div className="testimonials" id="testimonials">
+            <h1>Testimonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/right-arrow.png" className="left" alt="" />
+                        <img src="https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+                        <img src="assets/youtube.png" alt="" />
+                    </div>
+                    <div className="center"></div>
+                    <div className="bottom"></div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
