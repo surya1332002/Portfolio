eee2f52b12926ddf3e6415c24d6eea55de141292
Surya Vardhan Reddy Puchalapalli <Suryavardhanreddy13@gmail.com>
2022-06-04 15:28:14 +0530

    update color, and content (#4) (#5)
    * updated to new profile
    
    * added assets

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -19,7 +19,7 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/IMG_0242.png" alt=""/>
+                    <img src="assets/IMG_0493.jpg" alt=""/>
                 </div>
             </div>
             <div className="right">
